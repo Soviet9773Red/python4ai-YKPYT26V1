9d46c726ea8aa32d8aa3429253292c51b1d61418
Alexander <agabrielsten@gmail.com>
2026-03-04 00:29:42 +0100

    Update 02-01-04_Challange_Cond.py

--- week-02-control-flow-functions/02-01-04_Challange_Cond.py
+++ week-02-control-flow-functions/02-01-04_Challange_Cond.py
@@ -10,13 +10,13 @@ def grade_checker():
     print("Grade checker game.")
     grade = int(input("> Enter your grade (0 - 100): "))
     res = (
-        "Newbie\n" if grade >= 0  and grade <   20 else
-        "Rookie\n" if grade >= 20  and grade <   40 else
-        "Talented\n" if grade >= 40  and grade <   55 else
-        "Skilled\n" if grade >= 55  and grade <   70 else
-        "Advanced\n" if grade >= 70  and grade <  95 else
-        "Expert\n" if grade >= 95 and grade <= 100 else
-        "wrong input!\n"
+        "Newbie" if grade >= 0  and grade <   20 else
+        "Rookie" if grade >= 20  and grade <   40 else
+        "Talented" if grade >= 40  and grade <   55 else
+        "Skilled" if grade >= 55  and grade <   70 else
+        "Advanced" if grade >= 70  and grade <  95 else
+        "Expert" if grade >= 95 and grade <= 100 else
+        "wrong input!"
         )
     print("> Your level is: " + res if res!="wrong input!"  else res)
 
@@ -25,8 +25,8 @@ def three_num():
     list1 = []
     print("> Three numbers (n1-n3) game.")
     for n in range(3):
-        list1.append( int(input(f"Enter n{n}: ")) )
-    print (f"Max: {max(list1)} / Min: {min(list1)} / Avg: {sum(list1)/3:.4f} \n")
+        list1.append( float(input(f"Enter n{n}: ")) )
+    print (f"Max: {max(list1)} / Min: {min(list1)} / Avg: {sum(list1)/3:.2f}")
 
 # 3. Leap year
     # Every year that is exactly divisible by four is a leap year, 
@@ -35,10 +35,10 @@ def three_num():
     # For example, the years 1700, 1800, and 1900 were not leap years, but the years 1600 and 2000 were.
 def leap_y():
     print("> Leap year game")
-    y = int(input("Take a leap! Enter year: "))
-    print("This year is leap\n"
+    y = int(input("Take a leap! Enter year: "))   
+    print("This year is leap"
           if (y % 400 == 0 or (y % 4 == 0 and y % 100 != 0)) else 
-          "This year not leap\n")
+          "This year not leap")
 
 # 0. Show menu
 def show_menu():
@@ -54,10 +54,9 @@ def main():
     show_menu()
     menu = True
     while menu:
-        # print(
-        #     "0. show menu > "
-        # )
+
         choice = input("menu(0):> ")
+
         (
             show_menu() if choice == "0" else
             grade_checker() if choice == "1" else
@@ -66,9 +65,7 @@ def main():
             print("> Exiting ...") if choice == "4" else
             print("> Invalid input") 
         )
-
+        print()  # empty line
         menu = False if choice == "4" else True
-
-
 
 main()
